445078c4e50198fba287b4d34e99e9296007df39
Nick Hebner <hebnern@gmail.com>
2014-04-10 21:38:48 -0700

    Use valid hostname.

--- config.rb
+++ config.rb
@@ -6,7 +6,7 @@ API_VERSION = 1
 HOST = '0.0.0.0'
 DEVICE_PORT = 31337
 WEB_PORT = 10080
-BREWBIT_API_URL = 'http://0.0.0.0:80/api'
+BREWBIT_API_URL = 'http://staging.brewbit.com/api'
 
 $LOAD_PATH << File.join( ROOT_PATH, '.' )
 $LOAD_PATH.unshift *Dir.glob( File.expand_path( './app/**/*' ) )
