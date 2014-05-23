92742eeb417bd2cb357a87f9069ac414d31ca514
Nick Hebner <hebnern@gmail.com>
2014-05-22 21:00:15 -0700

    Inspect the payload.

--- app/servers/device_connection.rb
+++ app/servers/device_connection.rb
@@ -49,7 +49,7 @@ class DeviceConnection < EM::Connection
     begin
       MessageHandler.handle self, payload
     rescue
-      Log.error "Message handler error for #{device_id} #{payload}"
+      Log.error "Message handler error for #{device_id} '#{payload.inspect}'"
       Log.error $!.inspect
       Log.error $@
       
